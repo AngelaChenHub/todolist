557fafc91bfd816cdc63267a6ba6a618d7c51d38
Angela Chen <chen.angelay@gmail.com>
2014-10-30 20:57:19 -0700

    make my page

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    render :partial => 'index'
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render json: {:success =>  true, :task => @task}
+    else
+      render json: {:success => false}
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description, :archived, :priority)
+  end
+end
+
+def archive
+  @task = Task.find(params[:id])
+  @task.archived = 1
+  if @task.save
+      render json: {:success =>  true, :task => @task}
+  else
+    render json: {:success => false}
+  end
+end
